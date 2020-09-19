e12fae0554a2ed0e70274d3cdc826f9a71ee0951
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-09-19 14:55:35 -0300

    Aprendo Ingles
    App simples, utilizando conceito de midias e abas para praticar.

---
+++ AprendaIngles/app/src/main/java/com/example/aprendaingls/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.aprendaingls;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+
+import com.example.aprendaingls.Fragments.BichosFragment;
+import com.example.aprendaingls.Fragments.NumerosFragment;
+import com.example.aprendaingls.Fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+    private SmartTabLayout smartTabLayout;
+    private  ViewPager viewPager;
+    private MediaPlayer mediaPlayer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewpagertab);
+        viewPager = findViewById(R.id.viewpager);
+
+
+
+        getSupportActionBar().setElevation(0);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Bichos", BichosFragment.class)
+                .add("Números", NumerosFragment.class)
+                .add("Vogais", VogaisFragment.class)
+                .create());
+
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) smartTabLayout;
+        viewPagerTab.setViewPager(viewPager);
+
+
+    }
+
+
+
+}
